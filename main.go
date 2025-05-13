66eb9217fac4b0ff5dca67cde5ee2562badbdad9
Bryant Spicer <konviction@gmail.com>
2025-05-13 14:44:58 -0400

    add hit counter: ch1-14

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathroot = "."
 	const port = "8080"
 
+	// First, create an instance of apiConfig
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir(filepathroot))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
-	mux.HandleFunc("/healthz", handlerRediness)
 
+	// Then use the instance of call the middleware method
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/healthz", handlerRediness)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -27,4 +45,22 @@ func handlerRediness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	// get the count and format it correctcly
+	hitCount := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hitCount)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	// Reset the hit counter
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Counter reset"))
 }
