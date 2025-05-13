911499811cb5cd78019f8682b322132044fb5d04
Bryant Spicer <konviction@gmail.com>
2025-05-13 10:25:49 -0400

    added a health check endpoint: ch1-11

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
