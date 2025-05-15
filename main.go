36252d318ce7232eca6be19912707c014cbd1e2e
Bryant Spicer <konviction@gmail.com>
2025-05-15 10:29:27 -0400

    added a validatechirp api endpoint: ch4-2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -32,6 +33,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerRediness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChirpHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -68,4 +70,64 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	// Reset the hit counter
 	cfg.fileserverHits.Store(0)
 	w.Write([]byte("Counter reset"))
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type ErrorResponse struct {
+		Error string `json:"error"`
+	}
+
+	type ValidResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		errorM := ErrorResponse{
+			Error: "Method not allowed",
+		}
+		writeJSONResponse(w, 405, errorM)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		errorM := ErrorResponse{
+			Error: "Something went wrong"}
+		log.Printf("Error decoding JSON: %v", err)
+		writeJSONResponse(w, 500, errorM)
+		return
+	}
+	// Validate the chirp
+	if len(params.Body) > 140 {
+		errorM := ErrorResponse{
+			Error: "Chirp is too long",
+		}
+		log.Printf("Chirp is too long")
+		writeJSONResponse(w, 400, errorM)
+		return
+	}
+	validM := ValidResponse{
+		Valid: true,
+	}
+	log.Printf("Chirp is valid")
+	writeJSONResponse(w, 200, validM)
+}
+
+func writeJSONResponse(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("Error marshaling JSON response: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(status)
+	w.Write(response)
 }
